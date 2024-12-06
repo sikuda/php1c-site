4268f50d042b1b91c12cdbcdc69e0c056c84077c
sikudamobile <ya.sikuda@yandex.ru>
2024-12-06 17:04:10 +0300

    make site

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Sikuda\Php1c;
+
+echo "Hello php1c";
+
+
---
+++ public/result1CCode.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Sikuda\Php1c;
+/**
+* Скрипт для обмена выполняемого кода по 1С (обработка UnitTests.epf)
+*
+*/
+//ini_set('display_errors', 0);
+//ini_set('display_startup_errors', 0);
+//error_reporting(0);
+
+if(isset($_GET["lang"])) $Language1C = $_GET["lang"];
+
+$str = 'Silence is gold';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$code = file_get_contents('php://input');
+
+	//echo $str;
+	//die();
+
+	$php1C = new Sikuda\Php1c\Php1C();
+
+	if(isset($_GET["code"])) $str = $php1C->evalCode($code);
+	else{	
+		if ($Language1C === 'en') {
+			$str = $php1C->evalCode($code, "Result");    
+		}
+		else{
+			$str = php1C->evalCode($code, "Результат"); 
+		} 
+	}
+}	
+
+//echo $php1C->toString1C($str);
+echo $str;
